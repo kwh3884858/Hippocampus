f3ca0fc203d1e87f08c57df8e34a1196d26437fd
Weihang Kong <kwh3884858@gmail.com>
2019-05-19 23:52:50 +0800

    Update: Add a unload scene demo in knife scene

---
+++ Assets/Scripts/KnifeAndDoorScripts/UnloadDemo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnloadDemo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnGUI()
+    {
+      if(  GUI.Button(new Rect(0,0,100,100),"Unload Scene"))
+        {
+            Skylight.SceneManager.Instance().UploadScene(SceneLookupEnum.SceneKnifeAndTransformDoor.ToString());
+
+        }
+    }
+}
