9ebf6d2ed8667987f0041a81381b176726ad5f39
jinze <q787581405@gmail.com>
2020-03-23 02:33:30 +0800

    RecordBranch

---
+++ Assets/Scripts/Controllers/Subsystems/Stroy/StoryRecorder.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+
+namespace Controllers.Subsystems.Story
+{
+    public class StoryRecorder
+    {
+        public StoryRecorder()
+        {
+            m_record = new Dictionary<string, List<StoryAction>>();
+        }
+        
+        public void PushRecord(string id, StoryAction action)
+        {
+            if (m_preID != id && m_record.ContainsKey(id))
+            {
+                id = DealTheSameID(id);
+            }
+            
+            if (!m_record.ContainsKey(id))
+            {
+                m_record[id]= new List<StoryAction>();
+            }
+
+            switch (action.Type)
+            {
+                case StoryActionType.Bold:
+                case StoryActionType.Color:
+                case StoryActionType.Font:
+                case StoryActionType.Waiting:
+                case StoryActionType.FontSize:
+                case StoryActionType.Picture:
+                case StoryActionType.PictureMove:
+                case StoryActionType.ChangeEffectMusic:
+                case StoryActionType.ChangeBGM:
+                    break;
+                case StoryActionType.Content:
+                    PushContent(id, action);
+                    break;
+                case StoryActionType.Name:
+                case StoryActionType.Jump:
+                    m_record[id].Add(action);
+                    break;
+            }
+        }
+
+        public List<StoryAction> GetRecords()
+        {
+            List<StoryAction> records = new List<StoryAction>();
+            foreach (var record in m_record)
+            {
+                records.AddRange(record.Value);
+            }
+
+            return records;
+        }
+
+        public void ClearRecords()
+        {
+            foreach (var record in m_record)
+            {
+                record.Value.Clear();
+            }
+            m_record.Clear();
+        }
+
+        private void PushContent(string id,StoryAction content)
+        {
+            int lastActionIndex = m_record[id].Count -1;
+            if (lastActionIndex <= 0)
+            {
+                return;
+            }
+
+            StoryAction action = m_record[id][lastActionIndex];
+            if (action.Type == StoryActionType.Content)
+            {
+                action.Content = content.Content;
+            }
+        }
+
+        private string DealTheSameID(string id)
+        {
+            int i = 1;
+            string newID = id;
+            while (m_record.ContainsKey(newID))
+            {
+                newID = id + "_Record_" + i;
+                i++;
+            }
+
+            return newID;
+        }
+
+        private string m_preID;
+        private Dictionary<string,List<StoryAction>> m_record;
+    }
+}
---
+++ Assets/Scripts/UI/Panels/StaticBoard/RecordPanel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Config;
+using Const;
+using Controllers.Subsystems.Story;
+using StarPlatinum;
+using TMPro;
+using UI.Panels.Element;
+using UI.Panels.Providers;
+using UI.Panels.Providers.DataProviders;
+using UI.Panels.Providers.DataProviders.GameScene;
+using UI.Panels.Providers.DataProviders.StaticBoard;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Panels.StaticBoard
+{
+    public class RecordPanel : UIPanel<UIDataProviderGameScene,DataProvider>
+    {
+        
+    }
+}
