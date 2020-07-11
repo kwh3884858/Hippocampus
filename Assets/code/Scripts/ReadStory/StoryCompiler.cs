6309e7e720548fbf6f18fb8826e1fc9063299164
Kong Weihang <kwh3884858@126.com>
2020-07-12 02:10:28 +0800

    Update unity client for tachie command

--- Assets/code/Scripts/ReadStory/StoryCompiler.cs
+++ Assets/code/Scripts/ReadStory/StoryCompiler.cs
@@ -4,366 +4,326 @@ using System.Text;
 
 namespace StarPlatinum.StoryCompile
 {
-    /// <summary>
-    /// Translate content to token
-    /// </summary>
-    public sealed class StoryCompiler
-    {
+	/// <summary>
+	/// Translate content to token
+	/// </summary>
+	public sealed class StoryCompiler
+	{
 
-        public enum CompilerState
-        {
-            Error,
-            StateString,
+		public enum CompilerState
+		{
+			Error,
+			StateString,
 
-            StateInstructor,        //key word
-            StateIdentity,          //variable
+			StateInstructor,        //key word
+			StateIdentity,          //variable
 
-            StateStartBracket,
-            StateStopBracket,
-            StateSlash,
-            StateOp,
-        };
+			StateStartBracket,      // <
+			StateStopBracket,       // >
+			StateSlash,             // /
+			StateOp,
+		};
 
-        public enum TokenType
-        {
-            TokenNone,
+		public enum TokenType
+		{
+			TokenNone,
 
-            TokenInstructor,         //key word
-            TokenIdentity,           //variable
+			TokenInstructor,         //key word
+			TokenIdentity,           //variable
 
-            TokenContent,
+			TokenContent,
 
-            TokenOpStartBracket,
-            TokenOpStopBracket,
-            TokenOpColon,
-            TokenOpSlash,
+			TokenOpStartBracket,     // <
+			TokenOpStopBracket,      // >
+			TokenOpColon,
+			TokenOpSlash,            // /
 
-        };
-        public class Token
-        {
-            public TokenType m_tokeType;
-            public string m_content;
+		};
+		public class Token
+		{
+			public TokenType m_tokeType;
+			public string m_content;
 
-            public Token()
-            {
-                m_tokeType = TokenType.TokenNone;
-            }
+			public Token ()
+			{
+				m_tokeType = TokenType.TokenNone;
+			}
 
-            public Token(Token token)
-            {
-                m_tokeType = token.m_tokeType;
-                m_content = token.m_content;
-            }
-        };
+			public Token (Token token)
+			{
+				m_tokeType = token.m_tokeType;
+				m_content = token.m_content;
+			}
+		};
 
-        public  StoryCompiler()
-        {
-            m_tokens = new List<Token>();
-        }
+		public StoryCompiler ()
+		{
+			m_tokens = new List<Token> ();
+		}
 
-        public List<Token> Compile(string word)
-        {
-            Lexer(word);
+		public List<Token> Compile (string word)
+		{
+			Lexer (word);
 
-            return Output();
-        }
+			return Output ();
+		}
 
 
-        void Lexer(string word)
-        {
-            Clear();
+		void Lexer (string word)
+		{
+			Clear ();
 
-            StringBuilder tmp = new StringBuilder();
+			StringBuilder tmp = new StringBuilder ();
 
-            int length = word.Length;
+			int length = word.Length;
 
-            for (int j = 0; j < length; j++)
-            {
-                switch (word[j])
-                {
-                    case '<':
+			for (int j = 0; j < length; j++) {
+				switch (word [j]) {
+				case '<':
 
-                        SwitchCompilerState(CompilerState.StateStartBracket);
+					SwitchCompilerState (CompilerState.StateStartBracket);
 
-                        break;
+					break;
 
-                    case '>':
+				case '>':
 
-                        SwitchCompilerState(CompilerState.StateStopBracket);
-                        break;
+					SwitchCompilerState (CompilerState.StateStopBracket);
+					break;
 
-                    case '/':
-                        SwitchCompilerState(CompilerState.StateSlash);
-                        break;
+				case '/':
+					SwitchCompilerState (CompilerState.StateSlash);
+					break;
 
-                    case ':':
-                        SwitchCompilerState(CompilerState.StateOp);
-                        break;
+				case ':':
+					SwitchCompilerState (CompilerState.StateOp);
+					break;
 
-                    default:
-                        SwitchCompilerState(CompilerState.StateString);
-                        break;
+				default:
+					SwitchCompilerState (CompilerState.StateString);
+					break;
 
 
-                }
-
-
-
-                if (m_currentState == m_lastState)
-                {
-                    //copy string
-
-                    tmp.Append(word[j]);
-
-                    continue;
-                }
-
-                CreateToken(ref tmp, m_lastState);
-
-                m_lastState = m_currentState;
-
-                //copy string
+				}
 
-                tmp.Append(word[j]);
 
-            }
 
+				if (m_currentState == m_lastState) {
+					//copy string
 
-            CreateToken(ref tmp, m_lastState);
-        }
+					tmp.Append (word [j]);
 
+					continue;
+				}
 
-        void CreateToken(ref StringBuilder aString, CompilerState lastState)
-        {
-            if (aString.Length == 0)
-            {
-                return;
-            }
+				CreateToken (ref tmp, m_lastState);
 
-            switch (lastState)
-            {
-                case CompilerState.Error:
-                    break;
-                case CompilerState.StateString:
-                    {
-                        Token token = CreateTokenByString(ref aString, TokenType.TokenContent);
-
-                        break;
-                    }
-                case CompilerState.StateInstructor:
-                    {
-                        if (m_currentState == CompilerState.StateOp)
-                        {
-                            Token token = CreateTokenByString(ref aString, TokenType.TokenInstructor);
-
-                        }
-                        else if (m_currentState == CompilerState.StateStopBracket)
-                        {
-                            Token token = CreateTokenByString(ref aString, TokenType.TokenInstructor);
-                        }
-
-                        break;
-                    }
-                case CompilerState.StateIdentity:
-                    {
-                        Token token = CreateTokenByString(ref aString, TokenType.TokenIdentity);
+				m_lastState = m_currentState;
 
-                        break;
-                    }
+				//copy string
 
-                case CompilerState.StateStartBracket:
-                    {
-                        if (m_currentState == CompilerState.StateInstructor)
-                        {
-                            Token token = CreateTokenByString(ref aString, TokenType.TokenOpStartBracket);
-                        }
-                        else if (m_currentState == CompilerState.StateSlash)
-                        {
-                            Token token = CreateTokenByString(ref aString, TokenType.TokenOpStartBracket);
-
-                        }
-
-
-                        break;
-                    }
-                case CompilerState.StateStopBracket:
-                    {
-                        Token token = CreateTokenByString(ref aString, TokenType.TokenOpStopBracket);
-
-                        break;
-                    }
-                case CompilerState.StateOp:
-                    {
-                        if (m_currentState == CompilerState.StateInstructor)
-                        {
-                            Token token = CreateTokenByString(ref aString, TokenType.TokenOpColon);
-                        }
-                        else if (m_currentState == CompilerState.StateStartBracket)
-                        {
-                            Token token = CreateTokenByString(ref aString, TokenType.TokenOpColon);
-                        }
-                        else if (m_currentState == CompilerState.StateIdentity)
-                        {
-                            Token token = CreateTokenByString(ref aString, TokenType.TokenOpColon);
-                        }
-                        break;
-                    }
-
-                case CompilerState.StateSlash:
-                    if (m_currentState == CompilerState.StateInstructor)
-                    {
-                        Token token = CreateTokenByString(ref aString, TokenType.TokenOpSlash);
-
-                    }
-
-                    break;
-                default:
-                    break;
-            }
-        }
-
-
-
-        void SwitchCompilerState(CompilerState nextState)
-        {
-            switch (nextState)
-            {
-                case CompilerState.Error:
-
-                    break;
+				tmp.Append (word [j]);
 
+			}
 
-                case CompilerState.StateString:
-                    if (m_currentState == CompilerState.StateStopBracket)
-                    {
-                        m_currentState = nextState;
-                    }
-                    else if (m_currentState == CompilerState.StateStartBracket)
-                    {
-                        m_currentState = CompilerState.StateInstructor;
-                    }
-                    else if (m_currentState == CompilerState.StateOp)
-                    {
-                        m_currentState = CompilerState.StateIdentity;
-                    }
-                    else if (m_currentState == CompilerState.StateSlash)
-                    {
-                        m_currentState = CompilerState.StateInstructor;
-                    }
 
+			CreateToken (ref tmp, m_lastState);
+		}
 
-                    break;
 
-                case CompilerState.StateInstructor:
+		void CreateToken (ref StringBuilder aString, CompilerState lastState)
+		{
+			if (aString.Length == 0) {
+				return;
+			}
 
-                    if (m_currentState == CompilerState.StateStartBracket)
-                    {
-                        m_currentState = nextState;
-                    }
-                    break;
+			switch (lastState) {
+			case CompilerState.Error:
+				break;
+			case CompilerState.StateString: {
+					Token token = CreateTokenByString (ref aString, TokenType.TokenContent);
 
+					break;
+				}
+			case CompilerState.StateInstructor: {
+					if (m_currentState == CompilerState.StateOp) {
+						Token token = CreateTokenByString (ref aString, TokenType.TokenInstructor);
 
-                case CompilerState.StateIdentity:
+					} else if (m_currentState == CompilerState.StateStopBracket) {
+						Token token = CreateTokenByString (ref aString, TokenType.TokenInstructor);
+					}
 
-                    if (m_currentState == CompilerState.StateOp)
-                    {
-                        m_currentState = nextState;
-                    }
+					break;
+				}
+			case CompilerState.StateIdentity: {
+					Token token = CreateTokenByString (ref aString, TokenType.TokenIdentity);
 
-                    break;
+					break;
+				}
 
-                case CompilerState.StateStartBracket:
-                    if (m_currentState == CompilerState.StateString)
-                    {
-                        m_currentState = nextState;
-                    }
-                    if (m_currentState == CompilerState.StateStopBracket)
-                    {
-                        m_currentState = nextState;
-                    }
+			case CompilerState.StateStartBracket: {
+					if (m_currentState == CompilerState.StateInstructor) {
+						Token token = CreateTokenByString (ref aString, TokenType.TokenOpStartBracket);
+					} else if (m_currentState == CompilerState.StateSlash) {
+						Token token = CreateTokenByString (ref aString, TokenType.TokenOpStartBracket);
 
-                    break;
+					}
 
-                case CompilerState.StateStopBracket:
-                    if (m_currentState == CompilerState.StateIdentity)
-                    {
-                        m_currentState = nextState;
-                    }
 
-                    if (m_currentState == CompilerState.StateInstructor)
-                    {
-                        m_currentState = nextState;
-                    }
+					break;
+				}
+			case CompilerState.StateStopBracket: {
+					Token token = CreateTokenByString (ref aString, TokenType.TokenOpStopBracket);
 
-                    break;
+					break;
+				}
+			case CompilerState.StateOp: {
+					if (m_currentState == CompilerState.StateInstructor) {
+						Token token = CreateTokenByString (ref aString, TokenType.TokenOpColon);
+					} else if (m_currentState == CompilerState.StateStartBracket) {
+						Token token = CreateTokenByString (ref aString, TokenType.TokenOpColon);
+					} else if (m_currentState == CompilerState.StateIdentity) {
+						Token token = CreateTokenByString (ref aString, TokenType.TokenOpColon);
+					}
+					break;
+				}
 
-                case CompilerState.StateOp:
-                    if (m_currentState == CompilerState.StateInstructor)
-                    {
-                        m_currentState = nextState;
-                    }
-                    if (m_currentState == CompilerState.StateStartBracket)
-                    {
-                        m_currentState = nextState;
-                    }
+			case CompilerState.StateSlash:
+				if (m_currentState == CompilerState.StateInstructor) {
+					Token token = CreateTokenByString (ref aString, TokenType.TokenOpSlash);
 
-                    break;
+				}
 
+				break;
+			default:
+				break;
+			}
+		}
 
-                case CompilerState.StateSlash:
-                    if (m_currentState == CompilerState.StateStartBracket)
-                    {
-                        m_currentState = nextState;
-                    }
 
-                    break;
-                default:
-                    break;
-            }
 
-            //m_lastState = m_state;
-        }
+		void SwitchCompilerState (CompilerState nextState)
+		{
+			switch (nextState) {
+			case CompilerState.Error:
 
+				break;
 
 
-        List<Token> Output()
-        {
-            List<Token> output = new List<Token>(m_tokens);
+			case CompilerState.StateString:
+				if (m_currentState == CompilerState.StateStopBracket) {
+					m_currentState = nextState;
+				} else if (m_currentState == CompilerState.StateStartBracket) {
+					m_currentState = CompilerState.StateInstructor;
+				} else if (m_currentState == CompilerState.StateOp) {
+					m_currentState = CompilerState.StateIdentity;
+				} else if (m_currentState == CompilerState.StateSlash) {
+					m_currentState = CompilerState.StateInstructor;
+				}
 
-            Clear();
 
-            return output;
-        }
+				break;
 
-        Token CreateTokenByString(ref StringBuilder stringBuilder, TokenType tokenType)
-        {
-            Token token = new Token();
+			case CompilerState.StateInstructor:
 
-            token.m_content = stringBuilder.ToString();
-            stringBuilder.Clear();
-            token.m_tokeType = tokenType;
+				if (m_currentState == CompilerState.StateStartBracket) {
+					m_currentState = nextState;
+				}
+				break;
 
-            AddToken(token);
 
-            return token;
-        }
+			case CompilerState.StateIdentity:
 
-        void AddToken(Token token)
-        {
-            m_tokens.Add(token);
-        }
+				if (m_currentState == CompilerState.StateOp) {
+					m_currentState = nextState;
+				}
 
-        void Clear()
-        {
-            m_tokens.Clear();
-            m_currentState = CompilerState.StateString;
-            m_lastState = CompilerState.StateString;
-        }
+				break;
 
-        List<Token> m_tokens;
-        CompilerState m_currentState;
-        CompilerState m_lastState;
+			case CompilerState.StateStartBracket:
+				if (m_currentState == CompilerState.StateString) {
+					m_currentState = nextState;
+				}
+				if (m_currentState == CompilerState.StateStopBracket) {
+					m_currentState = nextState;
+				}
 
+				break;
 
-    }
+			case CompilerState.StateStopBracket:
+				if (m_currentState == CompilerState.StateIdentity) {
+					m_currentState = nextState;
+				}
+
+				if (m_currentState == CompilerState.StateInstructor) {
+					m_currentState = nextState;
+				}
+
+				break;
+
+			case CompilerState.StateOp:
+				if (m_currentState == CompilerState.StateInstructor) {
+					m_currentState = nextState;
+				}
+				if (m_currentState == CompilerState.StateStartBracket) {
+					m_currentState = nextState;
+				}
+
+				break;
+
+
+			case CompilerState.StateSlash:
+				if (m_currentState == CompilerState.StateStartBracket) {
+					m_currentState = nextState;
+				}
+
+				break;
+			default:
+				break;
+			}
+
+			//m_lastState = m_state;
+		}
+
+
+
+		List<Token> Output ()
+		{
+			List<Token> output = new List<Token> (m_tokens);
+
+			Clear ();
+
+			return output;
+		}
+
+		Token CreateTokenByString (ref StringBuilder stringBuilder, TokenType tokenType)
+		{
+			Token token = new Token ();
+
+			token.m_content = stringBuilder.ToString ();
+			stringBuilder.Clear ();
+			token.m_tokeType = tokenType;
+
+			AddToken (token);
+
+			return token;
+		}
+
+		void AddToken (Token token)
+		{
+			m_tokens.Add (token);
+		}
+
+		void Clear ()
+		{
+			m_tokens.Clear ();
+			m_currentState = CompilerState.StateString;
+			m_lastState = CompilerState.StateString;
+		}
+
+		List<Token> m_tokens;
+		CompilerState m_currentState;
+		CompilerState m_lastState;
+
+
+	}
 }
