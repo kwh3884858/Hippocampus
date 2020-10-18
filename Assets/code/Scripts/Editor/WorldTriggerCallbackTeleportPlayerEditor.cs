31dad9801ebd4892bf85287accb0e7e4ab1d63ce
FREDERICK\whkong <kwh3884858@gmail.com>
2020-10-18 17:52:21 +0800

    Teleport now can open related scene quickly.

--- Assets/code/Scripts/Editor/WorldTriggerCallbackTeleportPlayerEditor.cs
+++ Assets/code/Scripts/Editor/WorldTriggerCallbackTeleportPlayerEditor.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using Config.GameRoot;
+using GamePlay.Stage;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+using DesperateDevs.Unity.Editor;
 
 namespace GamePlay.EventTrigger
 {
@@ -47,6 +52,29 @@ namespace GamePlay.EventTrigger
 					}
 					GUI.backgroundColor = Color.white;
 				}
+
+				if (GUILayout.Button("Open teleport scene"))
+				{
+                    SceneLookupEnum sceneLookupEnum = trigger.GetTeleportScene();
+
+                    if (sceneLookupEnum != SceneLookupEnum.World_Invalid)
+                    {
+                        string sceneName = sceneLookupEnum.ToString();
+                        string folderName = sceneName.Substring(sceneName.IndexOf('_') + 1);
+
+                        bool isExistMissionScene = MissionSceneManager.Instance.IsFileMissionSceneExistInAssets(folderName, sceneName);
+                        if (isExistMissionScene)
+                        {
+                            string pathToScene = MissionSceneManager.Instance.GenerateFullScenePath(folderName, sceneName);
+                            Scene currentMissionScene = EditorSceneManager.OpenScene(pathToScene, OpenSceneMode.Single);
+                            EditorSceneManager.SetActiveScene(currentMissionScene);
+                        }
+                        else
+                        {
+                            EditorUtility.DisplayDialog("Scene Not Exist", "Make sure scene enum is valid. You should use SceneGenerator to re-generate the scene lookup file", "Ok");
+                        }
+                    }
+                }
 			}
 
 			if (GUI.changed) {
