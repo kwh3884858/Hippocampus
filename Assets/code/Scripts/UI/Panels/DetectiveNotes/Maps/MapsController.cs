2f4fde99bd0b6c094505664add1f1d34ca44a3fa
Wywsui <550613009@qq.com>
2020-10-26 00:07:54 +0800

    update:detective notes add maps

---
+++ Assets/code/Scripts/UI/Panels/DetectiveNotes/Maps/MapsController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapsController : MonoBehaviour
+{
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Close()
+    {
+        gameObject.SetActive(false);
+    }
+}
