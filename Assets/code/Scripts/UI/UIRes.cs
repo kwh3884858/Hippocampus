762116ae0362427589460affb69b4f1805a5baee
JinzeZhang <q787581405@126.com>
2020-10-27 04:14:53 +0800

    争锋相对更新

--- Assets/code/Scripts/UI/UIRes.cs
+++ Assets/code/Scripts/UI/UIRes.cs
@@ -2,16 +2,14 @@
 {
     public class UIRes
     {
-        public static string SpecialBarrageFront = "UI_Judgment_SpecialBarrageFront";
-        public static string SpecialBarrageImg = "UI_Judgment_SpecialBarrage";
-        public static string SpecialBarrageBehind = "UI_Judgment_SpecialBarrageBehind";
-        public static string NormalBarrageFront = "UI_Judgment_NormalBarrageFront";
-        public static string NormalBarrageImg = "UI_Judgment_NormalBarrageImg";
-        public static string NormalBarrageBehind = "UI_Judgment_NormalBarrageBehind";
+        public static string SpecialBarrageFront = "UI_Judgment_Controversy_Textures_SpecialBarrageHead";
+        public static string SpecialBarrageImg = "UI_Judgment_Controversy_Textures_SpecialBarrageBody";
+        public static string NormalBarrageFront = "UI_Judgment_Controversy_Textures_NormalBarrageHead";
+        public static string NormalBarrageImg = "UI_Judgment_Controversy_Textures_NormalBarrageBody";
 
         public static string ScreenLeftBegin = "UI_Controversy_Textures_ScreenLeftBegin";
         public static string ScreenLeftWin = "UI_Controversy_Textures_ScreenWinLeft";
         public static string ScreenRightWin = "UI_Controversy_Textures_ScreenWinRight";
-        public static string ScreenLeftLose = "UI_Controversy_Textures_ScreenLoseLeft";
+        public static string ScreenLeftLose = "UI_Controversy_Textures_ScreenLeftBegin";
     }
 }
