77bb174ff00899fd623d14537f35158c25f47fe8
DESKTOP-27KCDVJ\Jett <879273541@qq.com>
2020-11-22 23:38:55 +0800

    -disable Timeline cut-in in start scene

--- Assets/code/Scripts/UI/Panels/AutoCreate/MGStartManuMainManuPanel.cs
+++ Assets/code/Scripts/UI/Panels/AutoCreate/MGStartManuMainManuPanel.cs
@@ -78,8 +78,10 @@ namespace UI.Panels
             UIManager.Instance().ActivatState(GameState.Battle);// 设置当前状态,wywtsest
             //UIManager.Instance().ShowStaticPanel(UIPanelType.UICommonGameplayPanel);// 显示助手UI
             //UIManager.Instance().ShowStaticPanel(UIPanelType.UICommonBookmarkPanel);// 显示书签
-            TimelineManager.Instance().PlayTimeline(TimelineEnum.StartScene);
-            PlayerController.Instance().SetMoveEnable(false);
+            //#region Timeline Enable
+            //TimelineManager.Instance().PlayTimeline(TimelineEnum.StartScene);
+            //PlayerController.Instance().SetMoveEnable(false);
+            //#endregion
             //StarPlatinum.PrefabManager.Instance.LoadScene (SceneLookupEnum.World_Episode2_Pier, UnityEngine.SceneManagement.LoadSceneMode.Additive);
         }
 
