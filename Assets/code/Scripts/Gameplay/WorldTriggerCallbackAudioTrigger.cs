f7c78df786453d877bbabdea4c681e025e088111
FREDERICK\whkong <kwh3884858@gmail.com>
2020-12-15 21:25:20 +0800

    - Move name space to gameplay. - reformat the indentation of the code

--- Assets/code/Scripts/Gameplay/WorldTriggerCallbackAudioTrigger.cs
+++ Assets/code/Scripts/Gameplay/WorldTriggerCallbackAudioTrigger.cs
@@ -8,48 +8,45 @@ using UI.Panels.Providers.DataProviders;
 using UnityEngine;
 using VLB;
 
-public class WorldTriggerCallbackAudioTrigger : WorldTriggerCallbackBase
+namespace GamePlay.EventTrigger
 {
-    [SerializeField]
-    public AudioSource m_audioSource;
-    public bool m_debugSetting = true;
-
-    [Header("AudioClip Setting")]
-    public AudioClip m_audioClip;
-
-    public float m_audioVolume = 1;
-    public bool m_isLoop = false;
-    public bool m_onlyTiriggerOnce;
-
-    public void Awake()
+    public class WorldTriggerCallbackAudioTrigger : WorldTriggerCallbackBase
     {
-        m_audioSource = this.gameObject.AddComponent<AudioSource>();
-        m_audioSource.playOnAwake = false;
-    }
-    protected override void AfterStart()
-    {
-#if UNITY_EDITOR
-        //Debug 3D Text
-        if (m_debugSetting)
+        [SerializeField]
+        public AudioSource m_audioSource;
+        public bool m_debugSetting = true;
+
+        [Header("AudioClip Setting")]
+        public AudioClip m_audioClip;
+
+        public float m_audioVolume = 1;
+        public bool m_isLoop = false;
+        public bool m_onlyTiriggerOnce;
+
+        public void Awake()
         {
-            WorldDebug3DTextManager.Instance.AddTextToGameobject("AudioTrigger", gameObject);
+            m_audioSource = this.gameObject.AddComponent<AudioSource>();
+            m_audioSource.playOnAwake = false;
         }
-#endif
-    }
-    protected override void Callback()
-    {
-        m_audioSource.PlayOneShot(m_audioClip,m_audioVolume);
-        if (m_onlyTiriggerOnce)
+        protected override void AfterStart()
         {
-            StartCoroutine(DelayedDestory(m_audioClip.length));
+            AddSceneDebugTextForGameobject($"AudioTrigger:{m_audioClip.name}");
         }
-    }
+        protected override void Callback()
+        {
+            m_audioSource.PlayOneShot(m_audioClip, m_audioVolume);
+            if (m_onlyTiriggerOnce)
+            {
+                StartCoroutine(DelayedDestory(m_audioClip.length));
+            }
+        }
 
-    IEnumerator DelayedDestory(float t)
-    {
-        BoxCollider Box = GetComponent<BoxCollider>();
-        Box.enabled = false;
-        yield return new WaitForSeconds(t);
-        Destroy(gameObject);
+        IEnumerator DelayedDestory(float t)
+        {
+            BoxCollider Box = GetComponent<BoxCollider>();
+            Box.enabled = false;
+            yield return new WaitForSeconds(t);
+            Destroy(gameObject);
+        }
     }
 }
