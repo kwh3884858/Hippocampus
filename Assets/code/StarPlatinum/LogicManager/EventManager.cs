d92c6b099eafe93f0572442687176bce6eb113ea
FREDERICK\whkong <kwh3884858@gmail.com>
2021-04-25 19:23:41 +0800

    Clear useless code.

--- Assets/code/StarPlatinum/LogicManager/EventManager.cs
+++ Assets/code/StarPlatinum/LogicManager/EventManager.cs
@@ -87,69 +87,6 @@ namespace StarPlatinum.EventManager
 				action?.Invoke(data);
 			}
 		}
-
-		//事件委托.
-		//public delegate void EventDelegate<T> (T e) where T : EventArgs;
-
-
-
-
-		//public T AddLogic<T> () where T : LogicBase
-		//{
-		//	var go = new GameObject ();
-		//	go.name = typeof (T).ToString ();
-		//	T t = go.AddComponent<T> ();
-		//	m_currentLogic = t;
-		//	go.transform.SetParent (transform);
-
-		//	t.LogicInit ();
-		//	t.LogicShow ();
-		//	return t;
-		//}
-
-		//public T GetLogic<T> () where T : LogicBase
-		//{
-		//	Transform logicTran = transform.Find (typeof (T).ToString ());
-		//	if (logicTran != null) {
-		//		T t = logicTran.GetComponent<T> ();
-		//		m_currentLogic = t;
-		//		t.LogicShow ();
-		//		return t;
-		//	} else {
-		//		return null;
-		//	}
-		//}
-
-		//public T OpenLogic<T> () where T : LogicBase
-		//{
-		//	if (m_currentLogic != null) {
-		//		m_currentLogic.LogicClose ();
-		//		m_currentLogic = null;
-		//	}
-
-		//	T t = null;
-		//	t = GetLogic<T> ();
-		//	if (t == null) {
-		//		t = AddLogic<T> ();
-		//	}
-
-		//	return t;
-		//}
-
-		//public void Notify (int eventId, System.Object vars = null)
-		//{
-		//	//Debug.Log ((SkylightStaticData.LogicType)eventId);
-		//	m_currentLogic.Notify (eventId, vars);
-		//	m_currentLogic.DoEvent (eventId, vars);
-		//}
-
-		//public void RegisterCallback (int nEventID, LogicBase.LogicEventHandler handler)
-		//{
-		//	m_currentLogic.RegisterCallback (nEventID, handler);
-		//}
-
-		//new one
-
 	}
 
 }
