12a87a51930963397f4ce131fbdc54e951f8a31d
FREDERICK\whkong <kwh3884858@gmail.com>
2021-08-28 01:12:20 +0800

    Add YAML file as config for generator. User can change config to adapt different projects as generic tool. Update git ingore: ignore package folder of NuGet.

---
+++ Tools/UnitySceneLookupGenerator/SceneLookupGenerator/GeneratorPathConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SceneLookupGenerator
+{
+    class GeneratorPathConfig
+    {
+        public string Path_Assets /*= "\\Assets"*/ { get; set; }
+        //Scene Lookup
+        public string Path_Data_To_World /*= "\\data\\graphics\\World";*/{ get; set; }
+        public string Path_Data_To_SceneLookupGenerator /*= "\\data\\tools\\SceneLookupGenerator";*/{ get; set; }
+        public string Path_Code_To_File_SceneLookupTemplate /*= "\\data\\tools\\SceneLookupGenerator\\SceneLookupTemplate.txt";*/{ get; set; }
+        public string Scene_Lookup_OutputName/* = "\\SceneLookup.cs";*/{ get; set; }
+
+        public void ForamtForUnixLikeSystem()
+        {
+            Path_Assets = Path_Assets.Replace('\\', '/');
+            Path_Data_To_World = Path_Data_To_World.Replace('\\', '/');
+            Path_Data_To_SceneLookupGenerator = Path_Data_To_SceneLookupGenerator.Replace('\\', '/');
+            Path_Code_To_File_SceneLookupTemplate = Path_Code_To_File_SceneLookupTemplate.Replace('\\', '/');
+            Scene_Lookup_OutputName = Scene_Lookup_OutputName.Replace('\\', '/');
+        }
+    }
+}
--- Tools/UnitySceneLookupGenerator/SceneLookupGenerator/Program.cs
+++ Tools/UnitySceneLookupGenerator/SceneLookupGenerator/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
 
 namespace SceneLookupGenerator
 {
@@ -17,17 +22,7 @@ namespace SceneLookupGenerator
             Release
         }
 
-        private static DevMode DEV_MODE = DevMode.Release;
-        private static string Path_Assets = "\\Assets";
-        //Scene Lookup
-        private static string Path_Data_To_World = "\\data\\graphics\\World";
-        private static string Path_Data_To_SceneLookupGenerator = "\\data\\tools\\SceneLookupGenerator";
-        private static string Path_Code_To_File_SceneLookupTemplate = "\\data\\tools\\SceneLookupGenerator\\SceneLookupTemplate.txt";
-
-        private static string Path_Code_To_File_RootConfig = "\\code\\Scripts\\Config\\ConfigRoot.cs";
-        private static string Path_StarPlatinum_To_GameRootInspector = "\\code\\StarPlatinum\\Editor\\GameRootInspector.cs";
-        private static string Scene_Lookup_OutputName = "\\SceneLookup.cs";
-
+        private static DevMode DEV_MODE = DevMode.Debug;
         //Length of "\\Assets"
         private static readonly int PATH_ASSETS_LENGTH = 7;
 
@@ -43,7 +38,7 @@ namespace SceneLookupGenerator
 
                 if (DEV_MODE == DevMode.Debug)
                 {
-                    if (Environment.OSVersion.Platform == PlatformID.MacOSX)
+                    if (Environment.OSVersion.Platform == PlatformID.Unix || Environment.OSVersion.Platform == PlatformID.MacOSX)
                     {
                         exePath = "/Users/cookie/Documents/UnityProjectFolder/Hippocampus/Assets/data/tools/SceneLookupGenerator";
                     }
@@ -55,35 +50,60 @@ namespace SceneLookupGenerator
                 return exePath;
             }
         }
+
+        private static string configPath
+        {
+            get
+            {
+                string path = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+                if (Environment.OSVersion.Platform == PlatformID.Unix || Environment.OSVersion.Platform == PlatformID.MacOSX)
+                {
+                    path += "/GeneratorConfig.yaml";
+                }
+                else
+                {
+                    path += "\\GeneratorConfig.yaml";
+                }
+
+                return path;
+            }
+        }
         /// <summary>
         /// Program
         /// </summary>
 
         static SceneLookupGenerator m_sceneLookupGenerator;
-        static SceneConfigGenerator m_sceneConfigGenerator;
 
         //Example: -s /Users/cookie/Documents/UnityProjectFolder/BladeSlayer/Assets/Scenes -o /Users/cookie/Documents/UnityProjectFolder/BladeSlayer/Assets/Scenes
         public static void Main(string[] args)
         {
             //Initialize a generator
             m_sceneLookupGenerator = new SceneLookupGenerator();
-            m_sceneConfigGenerator = new SceneConfigGenerator();
+
+            StringBuilder content = new StringBuilder();
+            //Read template
+            using (FileStream fs = File.OpenRead(configPath))
+            {
+                byte[] b = new byte[1024];
+                UTF8Encoding temp = new UTF8Encoding(true);
+                while (fs.Read(b, 0, b.Length) > 0)
+                {
+
+                    content.Append( temp.GetString(b));
+                    Array.Clear(b, 0, b.Length);
+                }
+            }
+            IDeserializer deserializer = new DeserializerBuilder()
+                .Build();
+
+            GeneratorPathConfig config = deserializer.Deserialize<GeneratorPathConfig>(content.ToString());
 
             Console.WriteLine("Use -h for help info");
 
             if (Environment.OSVersion.Platform == PlatformID.Unix ||
                 Environment.OSVersion.Platform == PlatformID.MacOSX)
             {
-                Path_Assets = Path_Assets.Replace('\\', '/');
-
-                Path_Data_To_World = Path_Data_To_World.Replace('\\', '/');
-                Path_Data_To_SceneLookupGenerator = Path_Data_To_SceneLookupGenerator.Replace('\\', '/');
-                Path_Code_To_File_SceneLookupTemplate = Path_Code_To_File_SceneLookupTemplate.Replace('\\', '/');
-
-                Path_Code_To_File_RootConfig = Path_Code_To_File_RootConfig.Replace('\\', '/');
-                Path_StarPlatinum_To_GameRootInspector = Path_StarPlatinum_To_GameRootInspector.Replace('\\', '/');
-                Scene_Lookup_OutputName = Scene_Lookup_OutputName.Replace('\\', '/');
-
+                config.ForamtForUnixLikeSystem();
             }
 
             //Argument:
@@ -95,59 +115,35 @@ namespace SceneLookupGenerator
 
             ErrorType error = ErrorType.NoError;
 
-            m_sceneLookupGenerator.SetSceneLookupOutputName(Scene_Lookup_OutputName);
+            m_sceneLookupGenerator.SetSceneLookupOutputName(config.Scene_Lookup_OutputName);
 
             // Default argument
             if (args.Length == 0)
             {
-                //string exePath = "None";
-                //if (DEV_MODE == DevMode.Release) {
-                //	exePath = Environment.CurrentDirectory;
-                //}
-
-                //if (DEV_MODE == DevMode.Debug) {
-                //	exePath = "D:\\GithubRepository\\Hippocampus\\Assets\\Scenes";
-                //}
-
-                int pos = exePath.LastIndexOf(Path_Assets);
+                int pos = exePath.LastIndexOf(config.Path_Assets);
                 if (pos < 0) return;
                 //sub string to "../Assets"
                 string configFath = exePath.Substring(0, pos + PATH_ASSETS_LENGTH);
 
                 //scene root folder "Assets\\data\\graphics\\World"
-                string worldRootPath = configFath + Path_Data_To_World;
+                string worldRootPath = configFath + config.Path_Data_To_World;
                 error = m_sceneLookupGenerator.SetWorldRootPath(worldRootPath);
                 CheckError(error);
 
                 //scene output folder "Assets\\data\\tools\\SceneLookupGenerator\\SceneLookup.cs"
-                string lookupOutputPath = configFath + Path_Data_To_SceneLookupGenerator;
+                string lookupOutputPath = configFath + config.Path_Data_To_SceneLookupGenerator;
                 error = m_sceneLookupGenerator.SetOutputPath(lookupOutputPath);
                 CheckError(error);
 
                 //lookup template folder "Assets\\data\\tools\\SceneLookupGenerator\\SceneLookupTemplate.txt"
-                string templatePath = configFath + Path_Code_To_File_SceneLookupTemplate;
+                string templatePath = configFath + config.Path_Code_To_File_SceneLookupTemplate;
                 error = m_sceneLookupGenerator.SetTemplateFile(templatePath);
-                CheckError(error);
-
-                //scene root folder "Assets\\data\\graphics\\World"
-                error = m_sceneConfigGenerator.SetSceneRootPath(worldRootPath);
-                CheckError(error);
-
-                //Output Path "Assets\\code\\Scripts\\Config\\ConfigRoot.cs"
-                string rootConfigOutputPath = configFath + Path_Code_To_File_RootConfig;
-                error = m_sceneConfigGenerator.SetOutputPath(rootConfigOutputPath);
-                CheckError(error);
-
-                //inspector path "Assets\\code\\StarPlatinum\\Editor\\GameRootInspector.cs"
-                string inspectorFath = configFath + Path_StarPlatinum_To_GameRootInspector;
-                error = m_sceneConfigGenerator.SetInspectorFile(inspectorFath);
                 CheckError(error);
             }
 
             // For argument contained
             for (int i = 0; i < args.Length; i += 2)
             {
-
                 error = ErrorType.NoError;
 
                 switch (args[i])
@@ -197,15 +193,8 @@ Generated scene lookup file will put in this path.
             }
 
             error = m_sceneLookupGenerator.Execute();
-
             CheckError(error);
 
-/*          Now the game does`t need the scene type.
-            
-            error = m_sceneConfigGenerator.Execute();
-
-            CheckError(error);
-*/
             Console.WriteLine("Generate Lookup Successful!");
         }
 
